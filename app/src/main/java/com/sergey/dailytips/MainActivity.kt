60f31cd69292e9cee2272471a7592463cc225f4d
Sergey <basergey4@gmail.com>
2026-03-23 13:30:34 +0400

    feat: populate list with 10 tips

--- app/src/main/java/com/sergey/dailytips/MainActivity.kt
+++ app/src/main/java/com/sergey/dailytips/MainActivity.kt
@@ -18,7 +18,15 @@ class MainActivity : AppCompatActivity() {
 
         val tips = listOf(
             Tip(1, R.string.tip1_title, R.string.tip1_desc, R.drawable.ic_launcher_background),
-            Tip(2, R.string.tip2_title, R.string.tip2_desc, R.drawable.ic_launcher_background)
+            Tip(2, R.string.tip2_title, R.string.tip2_desc, R.drawable.ic_launcher_background),
+            Tip(3, R.string.tip3_title, R.string.tip3_desc, R.drawable.ic_launcher_background),
+            Tip(4, R.string.tip4_title, R.string.tip4_desc, R.drawable.ic_launcher_background),
+            Tip(5, R.string.tip5_title, R.string.tip5_desc, R.drawable.ic_launcher_background),
+            Tip(6, R.string.tip6_title, R.string.tip6_desc, R.drawable.ic_launcher_background),
+            Tip(7, R.string.tip7_title, R.string.tip7_desc, R.drawable.ic_launcher_background),
+            Tip(8, R.string.tip8_title, R.string.tip8_desc, R.drawable.ic_launcher_background),
+            Tip(9, R.string.tip9_title, R.string.tip9_desc, R.drawable.ic_launcher_background),
+            Tip(10, R.string.tip10_title, R.string.tip10_desc, R.drawable.ic_launcher_background),
         )
 
         recyclerView.layoutManager = LinearLayoutManager(this)
